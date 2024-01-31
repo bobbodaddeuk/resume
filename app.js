761969cef0ee60b61acd55a706c09243ac39e5b2
TriumphOne <woo001232naver.com>
2024-01-31 17:23:02 +0900

    스키마 모델 작성

--- app.js
+++ app.js
@@ -13,3 +13,7 @@ const PORT = 3020;
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api", [UsersRouter, AuthRouter, DocumentsRouter]);
+
+app.listen(PORT, () => {
+   console.log(PORT, "포트로 서버가 열렸어요!");
+});
