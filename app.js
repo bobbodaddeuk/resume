cd9d4ea73f9f5edaff06b55d0b7c8510e855b6ac
TriumphOne <woo001232naver.com>
2024-02-01 23:53:43 +0900

    최종

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-const PORT = 3020;
+const PORT = process.env.DATABASE_PORT;
 
 app.use(express.json());
 app.use(cookieParser());
